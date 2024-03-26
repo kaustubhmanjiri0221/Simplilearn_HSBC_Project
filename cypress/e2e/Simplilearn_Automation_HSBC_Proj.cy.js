7d55f00ef4d2aec12aae1f15af7006a88deb5095
Kaustubh Atul Agrawal <75115639+kaustubhmanjiri0221@users.noreply.github.com>
2024-03-26 16:14:29 +0530

    Add files via upload

---
+++ cypress/e2e/Simplilearn_Automation_HSBC_Proj.cy.js
@@ -0,0 +1,13 @@
+describe('HSBC Project', () => {
+  beforeEach(() => {
+    cy.visit('https://www.hsbc.co.in/security/')
+  })
+  it('CheckText', ()=>{
+    cy.get('input[id="rememberMe"]').click()
+    cy.get('input[id="username"]').type("User1234\t{enter}")
+    cy.get('span[class="icon icon-circle-help-solid help-icon"]').click()
+    // Here.....
+    cy.get('span[class="icon icon-delete"]').click()
+  })
+})
+
